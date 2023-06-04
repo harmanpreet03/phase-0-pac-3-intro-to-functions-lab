db0c2ba487da4d0e8fbdc647ca1171440059e289
harmanpreet ghotra <harmanpreetghotra17@gmail.com>
2023-06-03 20:37:29 -0400

    string excercise complete

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,5 +1,29 @@
 require ( './root.js' );
 
+function shout(string) {
+  return string.toUpperCase();
+}
+function whisper(string) {
+  return string.toLowerCase();
+}
+function logShout(string){
+ console.log(string.toUpperCase());
+}
+
+function logWhisper(string){
+  console.log(string.toLowerCase())
+}
+
+function sayHiToHeadphonedRoommate(string){
+
+  if(string.toLowerCase() === string)
+    return "I can't hear you!";
+
+  else if (string === "Let's have dinner together!")
+    return "I would love to!";
+
+  return "YES INDEED!";
+}
 
 describe('shout(string)', function() {
   it('receives one argument and returns it in all caps', function() {
